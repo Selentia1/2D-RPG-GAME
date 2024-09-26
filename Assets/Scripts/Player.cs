97464bb57141f39d01ea0afaeaa053de60a3c6a0
Selentia1 <1483690162@qq.com>
2024-09-26 17:53:06 +0800

    9.26 work

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,32 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.Accessibility;
 
 public class Player : MonoBehaviour
 {
+    [Header("Move And Jump")]
     //角色的移动速度和跳跃力
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
-
-    //获取组件
-    [SerializeField] private Rigidbody2D rb;
-    private Animator animator;
-
+   
     //是否在移动
     [SerializeField] private bool isMoving;
 
-    //各个方向速度
-    [SerializeField] private float velocityX;
-    [SerializeField] private float velocityY;
+    //判断玩家跳跃次数来实现二段跳
+    [SerializeField] private int jumpTimes;
 
+    //用于存储移动跳跃时获得的瞬时速度
+    private float velocityMove;
+    private float velocityDash;
+
+    [Header("Dash Info")]
+    //冲刺计时器
+    [SerializeField] private float dashTimer;
+    //冲刺持续时间
+    [SerializeField] private float dashDuration;
+    //冲刺CD计时器
+    [SerializeField] private float dashCDTimer;
+    //冲刺冷却时间
+    [SerializeField] private float dashCoolDown;
+    //冲刺速度
+    [SerializeField] private float dashSpeed;
+    //冲刺状态
+    [SerializeField] private bool isDash;
+
+    [Header("Physical Component")]
+    //获取组件
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Collider2D collider2D;
+    private Animator animator;
+
+    [Header("Collider Check")]
     //图层遮罩用于检测是否接触地面，将需要检测的物体设置到Ground图层
     [SerializeField] private LayerMask layerMask_Ground;
     //检测射线最大距离
     [SerializeField] private float GroundCheckDistance;
     //判断玩家是否在地面
-    private bool isGround;
+    [SerializeField] private bool isGround;
+    
 
     //角色朝向
     private enum FaceDirection {
@@ -40,6 +64,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        collider2D = GetComponent<Collider2D>();
         animator = GetComponentInChildren<Animator>();
         
         rb.velocity = Vector2.zero;
@@ -47,6 +72,11 @@ public class Player : MonoBehaviour
         jumpForce = 15;
         direction = FaceDirection.RIGHT;
         GroundCheckDistance = 1.02f;
+        isGround = true;
+        jumpTimes = 0;
+        dashSpeed = 15;
+        dashDuration = 0.3f;
+        dashCoolDown = 1;                                    
     }
 
     // Update is called once per frame
@@ -54,32 +84,74 @@ public class Player : MonoBehaviour
     {
         Movement();
         GetInPut();
+        ActionTimer();
 
-        JumpController();
+        ColliderController();
         AnimatorControllers();
         FlipController();
 
     }
 
-    private void GetInPut()
+    //动作相关的计时器
+    private void ActionTimer()
     {
-        velocityX = Input.GetAxisRaw("Horizontal") * moveSpeed;
-
-        //按下空格人物跳跃
-        if (Input.GetKeyDown(KeyCode.Space))
+        //冲刺计时器
+        if (dashTimer > 0)
         {
-            Jump();
+            dashTimer -= Time.deltaTime;
+        }
+        else if (dashTimer < 0)
+        {
+            isDash = false;
+        }
+
+        if (dashCDTimer > 0)
+        {
+            dashCDTimer -= Time.deltaTime;
         }
     }
 
-    //人物水平移动
-    private void Movement()
+    //获得输入
+    private void GetInPut()
     {
-        rb.velocity = new Vector2(velocityX, rb.velocity.y);
+        velocityMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        
+        //按下空格人物跳跃
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FixedJumpTimes();
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Dash();
+        }
     }
 
-    //动画控制器
-    private void AnimatorControllers() {
+    //人物冲刺
+    private void Dash()
+    {
+        if (dashCDTimer <= 0) 
+        {
+            isDash = true;
+            dashTimer = dashDuration;
+            dashCDTimer = dashCoolDown;
+
+            if (direction == FaceDirection.LEFT)
+            {
+                velocityDash = -dashSpeed;
+            }
+            else if (direction == FaceDirection.RIGHT)
+            {
+                velocityDash = dashSpeed;
+            }
+        }
+    }
+
+    //人物移动
+    private void Movement()
+    {
         //人物移动状态控制
         //isMoving = (rb.velocity.x != 0);
         if (rb.velocity.x != 0)
@@ -90,15 +162,41 @@ public class Player : MonoBehaviour
         {
             isMoving = false;
         }
-        animator.SetBool("isMoving", isMoving);
+
+        //人物冲刺时获得额外的速度
+        if (dashTimer > 0)
+        {
+            rb.velocity = new Vector2(velocityDash, 0);
+        }
+        else {
+            rb.velocity = new Vector2(velocityMove, rb.velocity.y);
+        }
+        
     }
+
+    //动画控制器
+    private void AnimatorControllers() {
+        animator.SetBool("isMoving", isMoving);
+        animator.SetBool("isGround", isGround);
+        animator.SetFloat("velocityY", rb.velocity.y);
+        animator.SetBool("isDash", isDash);
+    }
+
+    //修正跳跃次数
+    private void FixedJumpTimes() {
+        if (isGround) {
+            jumpTimes = 0;
+        }
+    }
+
+
 
     //跳跃
     private void Jump() {
-        //在地面上才能起跳
-        if (isGround) {
+        //二段跳实现
+        if (isGround || jumpTimes<2) {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            
+            jumpTimes++;
         }
         
     }
@@ -118,8 +216,8 @@ public class Player : MonoBehaviour
         }
     }
 
-    //控制玩家能否跳跃
-    private void JumpController() {
+    //碰撞检测
+    private void ColliderController() {
         isGround = Physics2D.Raycast(transform.position, Vector2.down, GroundCheckDistance, layerMask_Ground);
     }
 
